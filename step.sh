08017fd35ae533096e2edf70d55761cbfb2203a5
Zirkler <zirklerchristof@gmail.com>
2018-03-04 18:40:14 +0800

    more error handling

--- step.sh
+++ step.sh
@@ -6,6 +6,14 @@
 # $binary_version_code := The version code of the new binary
 # $notes := notes for this version, e.g. commit message or changelog
 
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
 while [ $# -gt 0 ]; do
    if [[ $1 == *"--"* ]]; then
         v="${1/--/}"
@@ -19,6 +27,9 @@ m_appmanager_app_id=${appmanager_id}
 m_binary_path=${binary_path}
 m_binary_version_code=${binary_version_code}
 m_notes=${notes}
+
+
+
 
 if [[ -z "$m_appmanager_app_id" ]]; then
   echo "using command line parameter"
@@ -36,6 +47,7 @@ echo -e "* Binary Path: $m_binary_path"
 echo -e "* Version Code: $m_binary_version_code"
 echo -e "* Notes: $m_notes"
 
+
 # TODO: Check if all input arguments are set
 
 if ! type "jq" > /dev/null; then
@@ -48,43 +60,56 @@ fi
 
 
 # send and store the new version
-echo -e "\\n--------------------------------------------------------------------------------"
-postVersionPayload="{
+echo -e "\\n---------------------------POSTING NEW VERSION----------------------------------"
+postVersionPayload="{ 
   \"versionNr\": \"$m_binary_version_code\",
   \"notes\": \"$m_notes\",
   \"app\": {
     \"id\": \"$m_appmanager_app_id\"
   }
 }"
-postVersionResponse=$(curl -0 -X POST \
+postVersionResponse=$(curl -s -S -X POST \
   "$m_base_api_url"/apps/"$m_appmanager_app_id"/versions/ \
   -H "Cache-Control: no-cache" \
   -H "Content-Type: application/json" \
   -d "$postVersionPayload")
+
+echo "${cyn}Response:${end} "
 echo "cURL exit code: $?"
-echo -e "--------------------------------------------------------------------------------\\n"
-
-
 echo -e "Post Version Response:"
 echo "$postVersionResponse"
-
 # parse the new version id from the response body and make the string to a number
 newVersionId=$(echo "$postVersionResponse" | jq .id | bc)
 echo "New Version ID: $newVersionId"
 
+if [[ -z "$newVersionId" ]]; then
+  echo "${red}Something went wrong: New version id is empty.${end}"
+  exit 1
+fi
+
+echo -e "--------------------------------------------------------------------------------\\n"
+
 # send the actual binary for the newly created version
-echo -e "\\n--------------------------------------------------------------------------------"
-postFileResponse=$(curl -0 -X POST \
+echo -e "\\n---------------------------UPLOADING BINARY-------------------------------------"
+postFileResponse=$(curl -s -S -X POST \
   "$m_base_api_url"/version/"$newVersionId"/file/ \
   -H 'Cache-Control: no-cache' \
   -H 'Content-Type: multipart/form-data' \
   -H 'Postman-Token: 669faafe-6625-0ee1-4054-fee882074171' \
   -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \
   -F file=@"$m_binary_path")
+
+echo "${cyn}Response:${end} "
 echo "cURL exit code: $?"
+echo "$postFileResponse"
+
+
+fileUrl=$(echo "$postFileResponse" | jq .fileUrl | bc) 
+if [[ -z "$newVersionId" ]]; then
+  echo "${red}Something went wrong: Received file url is empty.${end}"
+  exit 1
+fi
 echo -e "--------------------------------------------------------------------------------\\\n"
 
 
-echo -e "\\nUpload File Response"
-echo "$postFileResponse"
-echo "Deployment done."
+echo "${grn}Deployment done.${end}"
